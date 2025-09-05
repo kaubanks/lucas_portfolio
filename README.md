0a04d7cc773e2cbeeecddb4986381641b608b828
kaubanks <105695375+kaubanks@users.noreply.github.com>
2025-09-05 22:51:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+<h1 align="center" id="title">Portfolio Website V1</h1>
+
+<p id="description">This portfolio is my digital command center — a terminal-inspired interface where users can explore my projects, scan my professional profile, and access my social links, all while navigating a sleek, hacker-style environment.</p>
+
+<h2>🚀 Demo</h2>
+
+[https://kaubanks.github.io/lucas_portfolio/](https://kaubanks.github.io/lucas_portfolio/)
+
+  
+  
+<h2>🧐 Features</h2>
+
+Here're some of the project's best features:
+
+*   Command Execution and Routing
+*   Command History Navigation
+*   Dynamic Content Printing
+*   Command Line Interface (CLI) Simplicity
+*   External Link Opening
+
+<h2>🛠️ Installation Steps:</h2>
+
+<p>1. Clone the repository:</p>
+
+```
+git clone https://github.com/kaubanks/lucas_portfolio
+```
+
+<p>2. Go to the directory:</p>
+
+```
+cd lucas_portfolio
+```
+
+<p>3. Open the index.html file in a web browser</p>
+
+  
+  
+<h2>💻 Built with</h2>
+
+Technologies used in the project:
+
+*   JavaScript
+*   HTML
+*   CSS
+*   Browser DOM
+
+<h2>🛡️ License:</h2>
+
+This project is licensed under the [MIT License](LICENSE)
---
+++ scripts/data.js
@@ -0,0 +1,87 @@
+const linkedin = "https://www.linkedin.com/in/kaubanks/";
+const github = "https://github.com/kaubanks/";
+const email = "mailto:lucasrespick@gmail.com";
+var today = new Date();
+
+var about = [
+  "<br>",
+  "Hey, I'm Lucas Kauleti! 👋",
+  "<br>",
+  "I'm a student at University of Dodoma, Dodoma,.",
+  "I love to build things that are useful and can have a positive impact on other people's lives.",
+  "Having said that, I'm always ready to work on quirky projects",
+  "I am always ready to explore new things and collaborate with others.",
+  "Gathering a lot of knowledge and insights.",
+  "Outside of the professional space, I’m focused on tech, trading, and analyzing market trends to stay ahead.",
+  "<br>",
+  "Hope you explore and enjoy this website! 🙏🏻",
+  "<br>",
+  "<br>",
+  "Welcome......",
+  "<br>",
+];
+
+ var banner = [
+  "<br>",
+  '<span class = "index">' + String(today) + "</span>",
+  '<span class="index">Lucas Kauleti </span>',
+  "<br>",
+                  "██╗  ██╗ █████╗ ██╗   ██╗     ██████╗ ",  
+                  "██║ ██╔╝██╔══██╗██║   ██║    ██╔═══██╗",  
+                  "█████╔╝ ███████║██║   ██║    ██║██ ██║",  
+                  "██╔═██╗ ██╔══██║██║   ██║    ██║   ██║",  
+                  "██║  ██╗██║  ██║╚██████╔╝    ╚██████╔╝",  
+                  "╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝      ╚═════╝ ",
+
+
+  "<br>",
+  '<span class="color2">Welcome to my Portfolio Website..</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  // '<span class="color2"></span>',
+  "<br>",
+];
+
+var help = [
+  "<br>",
+  '👦🏻  <span class="command">About</span>          About Me',
+  '🌏  <span class="command">Social</span>         Display social networks',
+  '🏗  <span class="command">Projects</span>       View coding projects',
+  '📧  <span class="command">Email</span>          Reach me through email',
+  '🧹  <span class="command">Clear</span>          Clear the terminal',
+  '❓  <span class="command">Help</span>           Open the help menu',
+  '🕰  <span class="command">History</span>        View command history',
+  '👋  <span class="command">Exit</span>           Close the tab and exit',
+  "<br>",
+];
+
+var social = [
+  "<br>",
+  "Enter the name of any of the following or simply click the links...",
+  "<br>",
+  'Linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/kaubanks' +
+    "</a>",
+  'Github         <a href="' +
+    github +
+    '" target="_blank">github/kaubanks' +
+    "</a>",
+  "<br>",
+];
+
+var projects = [
+  "<br>",
+  '<span class="command">1. Datahub Repository</span>',
+  "<br>",
+  "<br>",
+  '<span class="command">2. Ranch Management System',
+  "<br>",
+  "<br>",
+  '<span class="command">3. Dog_Cat Model ',
+  "<br>",
+  "<br>",
+  '<span class="command">4. Portfolio Website (This Website)</span>',
+  "<br>",
+  'Click the following links to access all my projects: <a href = "https://github.com/kaubanks/" target="_blank">(Github)</a>',
+  "<br>",
+];
